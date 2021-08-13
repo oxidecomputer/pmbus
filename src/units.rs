36c99c7c10643ff3570257104a4280d94c3aca61
Bryan Cantrill <bryan@oxide.computer>
2021-08-12 20:50:55 -0700

    add units

---
+++ src/units.rs
@@ -0,0 +1,11 @@
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Volts(pub f32);
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Millivolts(pub f32);
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Amperes(pub f32);
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Milliamps(pub f32);
